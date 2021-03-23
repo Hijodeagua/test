5b13b7065e03477ffe4268edc627a317b43d8162
Hijodeagua <73750349+Hijodeagua@users.noreply.github.com>
2021-03-23 18:16:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,15 +20,15 @@ def home():
     return render_template("test.html")
 
 
-@app.route("/api/bars")
-def bars():
-    results = db.session.query(Bar.brewery,Bar.name, Bar.lat, Bar.lon).all()
+@app.route("/api/project")
+def project():
+    results = db.session.query(project.brewery,project.name, project.lat, project.lon).all()
 
     hover_text = [result[1] for result in results]
     lat = [result[2] for result in results]
     lon = [result[3] for result in results]
 
-    bar_data = [{
+    project_data = [{
         "type": "scattergeo",
         "locationmode": "USA-states",
         "lat": lat,
@@ -44,7 +44,7 @@ def bars():
         }
     }]
 
-    return jsonify(bar_data)
+    return jsonify(project_data)
 
 if __name__ == "__main__":
     app.run()
