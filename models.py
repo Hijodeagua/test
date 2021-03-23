d3aadca32a4197f86839e8f97067e6f1e2e6e13f
Hijodeagua <73750349+Hijodeagua@users.noreply.github.com>
2021-03-23 18:33:47 -0500

    Update models.py

--- models.py
+++ models.py
@@ -2,10 +2,10 @@ def create_classes(db):
     class bars(db.Model):
         __tablename__ = 'project'
 
-        brewery_id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String(64))
-        lat = db.Column(db.Float)
-        lon = db.Column(db.Float)
+        brewery_id = db.Column(db.Varchar, primary_key=True)
+        name = db.Column(db.Varchar)
+        lat = db.Column(db.Decimal)
+        lon = db.Column(db.Decimal)
 
         def __repr__(self):
             return '<project %r>' % (self.name)
