97f6aff3162ae2aed14b6991de62146f7af5dc32
Hijodeagua <73750349+Hijodeagua@users.noreply.github.com>
2021-03-24 17:30:13 -0500

    Add files via upload

--- static/js/leaflet-heat.js
+++ static/js/leaflet-heat.js
@@ -111,4 +111,3 @@
     } }), L.heatLayer = function(t, i) {
       return new L.HeatLayer(t, i);
     };
-  
--- static/js/page_two.js
+++ static/js/page_two.js
@@ -1,37 +1,63 @@
 // var myMap = L.map("map", {
-//     center: [39.50, -98.35],
-//     zoom: 5
-//   });
-  
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
+//   center: [37.7749, -122.4194],
+//   zoom: 13
+// });
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+var geoData = "brew.geoJSON";
+
+var geojson;
+
+d3.json(geoData, function(data) {
+
+  geojson = L.heatLayer(data, {
+
+      valueProperty: "Name",
+
+  // Binding a pop-up to each layer
+  onEachFeature: function(feature, layer) {
+      layer.bindPopup("city " + feature.properties.city + "<br>Brewery Name:<br>" +
+        "$" + feature.properties.name);
+    }
+  }).addTo(myMap);   
+
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
   }).addTo(myMap);
+// <<<<<<< HEAD
   
-  var geoData = "data/brew.geoJSON";
+//   var geoData = "data/brew.geoJSON";
 
-  var geojson;
+//   var geojson;
   
-  d3.json(geoData, function(data) {
+//   d3.json(geoData, function(data) {
   
-    geojson = L.heatLayer(data, {
+//     geojson = L.heatLayer(data, {
 
-        valueProperty: "Name",
+//         valueProperty: "name",
   
-    // Binding a pop-up to each layer
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup("city " + feature.properties.city + "<br>Brewery Name:<br>" +
-          "$" + feature.properties.name);
-      }
-    }).addTo(myMap);   
+//     // Binding a pop-up to each layer
+//     onEachFeature: function(feature, layer) {
+//         layer.bindPopup("city " + feature.properties.city + "<br>Brewery Name:<br>" +
+//           "$" + feature.properties.name);
+//       }
+//     }).addTo(myMap);   
   
-    var heat = L.heatLayer(heatArray, {
-      radius: 20,
-      blur: 35
-    }).addTo(myMap);
+//     var heat = L.heatLayer(heatArray, {
+//       radius: 20,
+//       blur: 35
+//     }).addTo(myMap);
   
-  });
+//   });
+
+});
+
