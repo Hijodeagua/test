b4bf7af8e188d9d715452f040228aa442baa29df
Hijodeagua <73750349+Hijodeagua@users.noreply.github.com>
2021-03-23 19:35:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ db = SQLAlchemy(app)
 # create route that renders index.html template
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("Project_2.html")
+
+@app.route("/page_two")
+def page_two():
+    """go to Page two."""
+    return render_template("page_two.html")
 
 if __name__ == "__main__":
     app.run()
