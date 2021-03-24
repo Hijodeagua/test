9a76ece78c955eea78cdfe772d58e18fa2741015
Hijodeagua <73750349+Hijodeagua@users.noreply.github.com>
2021-03-24 17:19:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from models import create_classes
 import os 
 from flask import (Flask,render_template,jsonify,request,redirect)
-
-
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -19,7 +19,11 @@ app = Flask(__name__)
 def home():
     return render_template("Project_2.html")
 
-@app.route("/page_two")
+@app.route("/Project_2")
+def Project_2():
+    return render_template("Project_2.html")
+
+@app.route("/page_two.html")
 def page_two():
     return render_template("page_two.html")
 
