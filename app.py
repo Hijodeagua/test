42cd1be0217f3ba92277008626aaf5953b2ed450
Hijodeagua <73750349+Hijodeagua@users.noreply.github.com>
2021-03-24 15:42:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-topbar = Navbar(logo,
+topbar = Navbar(
                 View('home', 'get_home),
                 View('page_two', 'page_two'),
                 )
