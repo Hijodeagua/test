16349142523b9730230d1db4c2a445215c9c9793
Hijodeagua <73750349+Hijodeagua@users.noreply.github.com>
2021-03-23 19:27:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,34 +17,7 @@ db = SQLAlchemy(app)
 # create route that renders index.html template
 @app.route("/")
 def home():
-    return render_template("test.html")
-
-
-@app.route("/api/project")
-def project():
-    results = db.session.query(project.brewery,project.name, project.lat, project.lon).all()
-
-    hover_text = [result[1] for result in results]
-    lat = [result[2] for result in results]
-    lon = [result[3] for result in results]
-
-    project_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 20,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
-
-    return jsonify(project_data)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
